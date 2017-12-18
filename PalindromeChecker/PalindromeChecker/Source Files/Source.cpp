f1bbe174002bc900b2bbd1f621039622aa5bd225
A-VOID-A-VOID-A-VOID <34438014+A-VOID-A-VOID-A-VOID@users.noreply.github.com>
2017-12-18 17:01:05 +1100

    Created project

---
+++ PalindromeChecker/PalindromeChecker/Source Files/Source.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////
+// Author/s:			Chris Murphy
+// Date created:		18/12/17
+// Date last edited:	18/12/17
+//////////////////////////////////////////////////
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// The entry point of the program.
+int main()
+{
+	while (true)
+	{
+		cout << "Enter palindrome to be checked: ";
+
+		string input;
+		getline(cin, input);
+	}
+
+	system("pause");
+
+	return 0;
+}
