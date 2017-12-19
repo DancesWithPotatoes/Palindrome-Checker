a1dbb2dca793051df7c0ab6a30ba7b1f941354d3
A-VOID-A-VOID-A-VOID <34438014+A-VOID-A-VOID-A-VOID@users.noreply.github.com>
2017-12-19 16:03:53 +1100

    Started implementing palindrome checking

--- PalindromeChecker/PalindromeChecker/Source Files/Source.cpp
+++ PalindromeChecker/PalindromeChecker/Source Files/Source.cpp
@@ -1,12 +1,30 @@
 //////////////////////////////////////////////////
 // Author/s:			Chris Murphy
 // Date created:		18/12/17
-// Date last edited:	18/12/17
+// Date last edited:	19/12/17
 //////////////////////////////////////////////////
+#define MAX_PALINDROME_LENGTH 256
 #include <iostream>
+#include <exception>
+#include <sstream>
 #include <string>
 
 using namespace std;
+
+// Returns whether or not the provided string is a valid palindrome.
+bool GetIfValidPalindrome(const string& str)
+{
+	// Ensures that the length of the provided string is valid.
+	if (str.length() < 2 || str.length() > MAX_PALINDROME_LENGTH)
+	{
+		// The string stream used to create the error message of the exception to throw.
+		ostringstream errorMsg;
+		errorMsg << "Error - the length of the palindrome string must be greater than 1 character and less than " << MAX_PALINDROME_LENGTH << " characters.";
+		throw invalid_argument(errorMsg.str());
+	}
+
+	return false;
+}
 
 // The entry point of the program.
 int main()
@@ -15,11 +33,23 @@ int main()
 	{
 		cout << "Enter palindrome to be checked: ";
 
+		// The string used to store the input of the user.
 		string input;
 		getline(cin, input);
+
+		try
+		{
+			GetIfValidPalindrome(input);
+		}
+		catch (invalid_argument& e)
+		{
+			cout << e.what() << endl;
+		}	
+
+		cout << endl; 
 	}
 
-	system("pause");
+	// TODO - add valid exit input which breaks the while loop.
 
 	return 0;
 }
